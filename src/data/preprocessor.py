7124c01f7989a4892130c246a651c7fb9f0deea1
SudhanshuChahal <sudhanshuchahal262@gmail.com>
2025-02-12 05:06:34 +0530

    Preprocessing of data done.

--- src/data/preprocessor.py
+++ src/data/preprocessor.py
@@ -1,11 +1,35 @@
-from sklearn.preprocessing import MinMaxScaler
+from sklearn.preprocessing import MultiLabelBinarizer
+import pandas as pd
+from sklearn.feature_extraction.text import TfidfVectorizer
 
 
-def clean_ratings():
-    ratings_df = ratings_df.drop(coulumns=['timestamp'])
+def clean_ratings(ratings_df):
+    ratings_clean = ratings_df.drop(columns=['timestamp'])
 
-def clean_movies():
-    pass
+    return ratings_clean
+ 
 
-def merge_data():
-    pass
+def clean_movies(movies_df):
+    movies_clean = movies_df.copy()
+
+    movies_clean['genres'] =movies_clean['genres'].str.split('|')
+
+    movies_clean['year'] = movies_clean['title'].str.extract(r'\((\d{4})\)')
+    movies_clean['year'] = pd.to_numeric(movies_clean['year'], errors='coerce')
+
+    movies_clean['title'] = movies_clean['title'].str.replace(r'\s*\(\d{4}\)','',regex=True)
+
+    mlb = MultiLabelBinarizer()
+    genres_encoded = pd.DataFrame(mlb.fit_transform(movies_clean['genres']),columns=mlb.classes_)
+    movies_clean = pd.concat([movies_clean, genres_encoded],axis=1)
+
+    tfidf =TfidfVectorizer(stop_words='english')
+    tfidf_matrix = tfidf.fit_transform(movies_clean['genres'].apply(lambda x: ' '.join(x)))
+
+    return movies_df,tfidf_matrix
+
+
+def merge_data(ratings_df, movies_df):
+    merged_df = pd.merge(ratings_df, movies_df, on='movieId', how='left')
+
+    return merged_df
--- src/main.py
+++ src/main.py
@@ -1,5 +1,7 @@
 from data.data_loader import load_rating_data, load_movie_metadata
+from data.preprocessor import clean_ratings, clean_movies, merge_data
 import os
+import pickle
 
 
 rating_path = os.path.join("..", "data", "raw","ratings.csv" )
@@ -10,7 +12,21 @@ def main():
     # Load data
     ratings = load_rating_data(rating_path)
     movies = load_movie_metadata(movies_path)
-    print("ratings data\n",ratings.head(),"\nmovies metadata\n",movies.head())
+    
+    #preprocess
+    ratings_clean =clean_ratings(ratings)
+    movies_clean, tfidf_matrix = clean_movies(movies)
+    merged_df = merge_data(ratings_clean, movies_clean)
+
+    #save processed data
+    ratings_clean.to_csv('../data/processed/ratings_clean.csv', index=False)
+    movies_clean.to_csv('../data/processed/movies_clean.csv', index=False)
+    merged_df.to_csv('../data/processed/merged_df.csv', index=False)
+
+    #save TF-IDF matrix
+    with open('../data/processed/tfidf_matrix.pkl','wb') as f:
+        pickle.dump(tfidf_matrix, f)
+
 
 
 if __name__ == '__main__':
